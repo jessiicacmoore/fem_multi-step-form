2b5692776c27c08de9d5123e36903fa4bda79bf2
Jessica Moore <jessica.cmoore1@gmail.com>
2025-07-27 20:53:07 -0400

    🔧 Add aliasing for imports

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,12 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@app': path.resolve(__dirname, 'src/app'),
+    },
+  },
   test: {
     projects: [{
       extends: true,
